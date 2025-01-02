0f8fe614813b82f5b43113b52fa96a5e29019a9c
Coessitompul <coessitompul99@gmail.com>
2025-01-02 17:02:01 +0700

    Video tutorial keempat Link & Navigation

--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -7,9 +7,9 @@ export default function DetailProductPage(props: DetailProductPageProps) {
       <h1>{params.slug ? "Detail Product Page" : "Product Page"}</h1>
       { params.slug && 
         <>
-          <h2>Category: {params.slug[0]}</h2>
-          <h2>Gender: {params.slug[1]}</h2>
-          <h2>Id: {params.slug[2]}</h2>
+          <h2>Category: {params?.slug[0]}</h2>
+          <h2>Gender: {params?.slug[1]}</h2>
+          <h2>Id: {params?.slug[2]}</h2>
         </>
       }
     </div>
