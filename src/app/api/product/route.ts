78f248420c68c6d07f4592195050a132671d6189
Coessitompul <coessitompul99@gmail.com>
2025-01-14 11:49:45 +0700

    Video tutorial ke 6 membahas mengenai Data Fetching dan Video tutorial ke 7 Mengenai Caching dan Revalidating, Revalidating sangat perlu untuk dipelajari lebih lanjut

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -3,14 +3,29 @@ import { NextRequest, NextResponse } from "next/server";
 const data = [
   {
     id: 1,
-    name: 'Sepatu Baru',
-    price: 100000
+    title: 'Air Jordan 1 Low',
+    price: 1729000,
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/AIR+FORCE+1+%2707.png",
   },
   {
     id: 2,
-    name: 'Baju Baru',
-    price: 200000
-  }
+    title: 'Air Jordan 1 Low',
+    price: 1729000,
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/32b0f17a-38ba-40fa-9de7-31c5bb1661e3/AIR+JORDAN+1+LOW.png",
+  },
+  {
+    id: 3,
+    title: 'Air Jordan 1 Mid',
+    price: 1909000,
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/89b9c5f5-9049-422d-aa76-19ea5323ef58/WMNS+AIR+JORDAN+1+MID.png",
+  },
+  {
+    id: 4,
+    title: 'Air Jordan 1 Mid',
+    price: 1909000,
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/89b9c5f5-9049-422d-aa76-19ea5323ef58/WMNS+AIR+JORDAN+1+MID.png",
+  },
+
 ]
 
 export async function GET(request: NextRequest) {
